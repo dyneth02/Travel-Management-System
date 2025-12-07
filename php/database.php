f349f2290ab40e51b69414946e74d498171bb927
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2025-12-08 00:25:42 +0530

    Add files via upload

--- php/database.php
+++ php/database.php
@@ -1,9 +1,9 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "boatsafari";
+    $host="localhost";
+    $user="root";
+    $pass="";
+    $dbname="boatsafari";
 
-$conn =mysqli_connect($host , $user , $pass , $dbname);
+    $conn=mysqli_connect($host, $user ,$pass, $dbname);
 
 ?>
